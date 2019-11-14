899038eece5169c1ff53799a55b70d954451d789
Damir Bulic <dbulic@spectralcore.com>
2019-11-14 12:40:32 +0100

    Initial

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,46 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light mb-5">
+      <router-link class="navbar-brand" to="/">Our App</router-link>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li>Todo stranica</li>
+        </ul>
+        <router-link v-if="!authenticated" class="btn btn-info my-2 my-sm-0 mr-2" to="/login">Login</router-link>
+        <span v-if="authenticated">
+          {{ userName }}
+          <a @click="logout" class="btn btn-info my-2 my-sm-0 mr-2" href="#">Logout</a>
+        </span>
+        <router-link class="btn btn-outline my-2 my-sm-0 mr-2" to="/signup">Signup</router-link>
+      </div>
+    </nav>
+
     <router-view/>
+
+    <div id="footer" class="text-center mt-5">Ovo je footer. (c) 2019 mi svi skupa.</div>
   </div>
 </template>
+
+<script>
+import store from '@/store.js'
+
+export default {
+  data () {
+    return store;
+  },
+
+  methods: {
+    logout() {
+      store.authenticated = false
+    }
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
--- src/router/index.js
+++ src/router/index.js
@@ -11,13 +11,15 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/login',
+    name: 'login',
+    component: () => import(/* webpackChunkName: "login" */ '../views/Login.vue')
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: () => import(/* webpackChunkName: "signup" */ '../views/Signup.vue')
+  },
 ]
 
 const router = new VueRouter({
